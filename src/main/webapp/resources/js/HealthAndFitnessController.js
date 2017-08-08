708d1dd2cfcc837465a275d4ee4e01369d35c273
benwebsta <bmwebster@wisc.edu>
2017-08-08 10:55:30 -0400

    Health and Fitness page done

--- src/main/webapp/resources/js/HealthAndFitnessController.js
+++ src/main/webapp/resources/js/HealthAndFitnessController.js
@@ -1,7 +1,77 @@
 app.controller("HealthAndFitnessController", 
-	['$scope', '$http', 
-		function($scope, $http) {
+	['$scope', '$http', '$state', '$timeout', '$rootScope',
+		function($scope, $http, $state, $timeout, $rootScope) {
+		$rootScope.showHealthAndFitnessTable = false;
+		var responseArray;
+		$scope.getAllHealthAndFitnesses = function(){
+			console.log("in get all healthAndFitnesses");
+			$http({
+	    	  method: 'POST',
+	    	  url: 'getAllHealthAndFitnesses',
+	    	  data: $scope.user
+	    	}).then(function successCallback(response) {	
+	    		responseArray = response.data;
+	    		console.log("success callback");
+	    		$rootScope.healthAndFitnessEntries = response.data;
+	    	    
+	    	    if(response.data != "")
+	    	    	$rootScope.showHealthAndFitnessTable = true;
+	    	    	
+	    	    
+				$rootScope.healthAndFitnessLabels = new Array();
+				$rootScope.healthAndFitnessData = new Array();
+				$rootScope.healthAndFitnessLabels2 = new Array();
+				$rootScope.healthAndFitnessData2 = new Array();
+				$rootScope.healthAndFitnessSeries = ['HealthAndFitness'];
+				$rootScope.options = {legend: {display: true}};
+				
+				for(i = 0; i < responseArray.length; i++){
+					console.log(responseArray[i]);
+					$rootScope.healthAndFitnessLabels.push(responseArray[i].description);
+					$rootScope.healthAndFitnessData.push(responseArray[i].amount);
 
+					$rootScope.healthAndFitnessLabels2.push(responseArray[i].healthAndFitnessDate);
+					$rootScope.healthAndFitnessData2.push(responseArray[i].amount);
+				}
+				
+	    	  }, function errorCallback(response) {
+	    		  console.log("error");
+	    	   	  console.log(response.data);
+	    	  });
+			
+		}
 		
+		
+		$scope.healthAndFitnessEntrySubmit = function(){
+			var healthAndFitness = {
+					amount: $scope.amount,
+					description: $scope.description,
+					user: $scope.user
+			}
+			console.log("healthAndFitness controller entry click");
+			console.log(healthAndFitness);
+			
+	    	console.log("in POST");
+		    $http({
+		    	  method: 'POST',
+		    	  url: 'createNewHealthAndFitness',
+		    	  data: healthAndFitness
+		    	}).then(function successCallback(response) {
+		    	   $scope.result = response.data;
+		    	   console.log(response.data);
+					$scope.healthAndFitnessEntry = true;
+					$timeout(closeModal, 1000);
+					$scope.getAllHealthAndFitnesses();
+		    	  }, function errorCallback(response) {
+		    		  console.log("error");
+		    	   	  console.log(response.data);
+		    	  });
+		}
+		
+		var closeModal = function(){
+			$(".modal-body input").val("");
+			$('#healthAndFitnessModal').modal('hide');
+			$scope.healthAndFitnessEntry = null;
+		}
 		  
 	}]);
